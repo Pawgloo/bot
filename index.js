452552b31201fc6fecdd31799de8a1432eefbdbe
gaurav2361 <gaurav23617@gmail.com>
2026-02-12 13:44:12 +0530

    chore: code formated
    Signed-off-by: gaurav2361 <gaurav23617@gmail.com>

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
  * @param {import('probot').Probot} app
  */
 export default (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+	// Your code here
+	app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+	app.on("issues.opened", async (context) => {
+		const issueComment = context.issue({
+			body: "Thanks for opening this issue!",
+		});
+		return context.octokit.issues.createComment(issueComment);
+	});
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+	// For more information on building apps:
+	// https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+	// To get your app running against GitHub, see:
+	// https://probot.github.io/docs/development/
 };
